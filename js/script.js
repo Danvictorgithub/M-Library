c5e60379cf83f5b623afae47cee241cef25763db
Dan Victor <danvictorprogramming1@gmail.com>
2022-05-16 23:05:52 +0800

    Added a Functional modal

--- js/script.js
+++ js/script.js
@@ -9,3 +9,16 @@ function Book(title, author,numPages,isRead) {
 Book.prototype.info = function () {
   return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.isRead}`;
 }
+
+// Selectors
+const modal = document.querySelector('.modal-container');
+const modalButtonOpen = document.querySelector('.modal-open').addEventListener('click', () => {
+  modal.classList.remove('hide');
+});
+const modalButtonClose = document.querySelector('.modal-close').addEventListener('click', () => {
+  modal.classList.add('hide');
+});
+
+modal.addEventListener('click',(e) => {
+  console.log(e.target.classList.add('hide'));
+})
